6c2a2096eb7b278df727afb95a51a416a8168d67
RockFeng <RockFeng@DESKTOP-SF06DHK.lan>
2018-07-24 22:05:53 +0800

    support github install requiremment

---
+++ httpdriver/__about__.py
@@ -0,0 +1,29 @@
+#! python3
+# -*- encoding: utf-8 -*-
+'''
+Current module: httpdriver.__about__
+
+Rough version history:
+v1.0    Original version to use
+
+********************************************************************
+    @AUTHOR:  Administrator-Bruce Luo(罗科峰)
+    MAIL:    lkf20031988@163.com
+    RCS:      httpdriver.__about__,v 1.0 2018年7月24日
+    FROM:   2018年7月24日
+********************************************************************
+
+======================================================================
+
+UI and Web Http automation frame for python.
+
+'''
+
+__title__           = "httpdriver"
+__short_desc__      = "only for http test, base on rtsf"
+__version__         = 1.0
+__autor__           = u"罗科峰"
+__author_email__    = "lkf20031988@163.com"
+__license__         = 'MIT'
+
+HOME_PAGE           = "https://github.com/RockFeng0/rtsf-http"
---
+++ setup.py
@@ -0,0 +1,47 @@
+#! python3
+# -*- encoding: utf-8 -*-
+'''
+Current module: setup
+
+Rough version history:
+v1.0    Original version to use
+
+********************************************************************
+    @AUTHOR:  Administrator-Bruce Luo(罗科峰)
+    MAIL:    lkf20031988@163.com
+    RCS:      setup,v 1.0 2018年7月24日
+    FROM:   2018年7月24日
+********************************************************************
+
+======================================================================
+
+UI and Web Http automation frame for python.
+
+'''
+from httpdriver import __about__
+from setuptools import setup, find_packages
+
+install_requires = [
+    "requests",
+    "rtsf",
+]
+
+dependency_links=[
+"git+https://github.com/RockFeng0/rtsf.git#egg=rtsf-0"
+]
+
+setup(
+        name = __about__.__title__,
+        version=__about__.__version__,        
+        description=__about__.__short_desc__,
+        author=__about__.__autor__,
+        author_email=__about__.__author_email__,
+        url=__about__.HOME_PAGE,
+        license=__about__.__license__,
+        python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
+        packages=find_packages(exclude=()),        
+        keywords='test http api https',
+        install_requires=install_requires,
+        dependency_links=dependency_links,
+        extras_require={},
+    )
