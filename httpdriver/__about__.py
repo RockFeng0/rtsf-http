b1f6e1f8f3ecc8877a4e766c2aaf35b54833ec3d
rockfeng0 <lkf20031988@163.com>
2018-07-25 15:51:16 +0800

    version 1.1

--- httpdriver/__about__.py
+++ httpdriver/__about__.py
@@ -21,7 +21,7 @@ UI and Web Http automation frame for python.
 
 __title__           = "httpdriver"
 __short_desc__      = "only for http test, base on rtsf"
-__version__         = 1.0
+__version__         = 1.1
 __autor__           = u"罗科峰"
 __author_email__    = "lkf20031988@163.com"
 __license__         = 'MIT'
