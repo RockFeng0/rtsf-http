d9430994d1d2c0fe1b0bf77cfce159bfbe639918
RockFeng0 <lkf20031988@163.com>
2018-11-04 10:16:29 +0800

    update version to v2.1, support data-driven and locust client

--- setup.py
+++ setup.py
@@ -19,7 +19,55 @@ UI and Web Http automation frame for python.
 
 '''
 from httpdriver import __about__
-from setuptools import setup, find_packages
+import os, io, sys
+from shutil import rmtree
+from setuptools import setup, find_packages, Command
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with io.open("README.md", encoding='utf-8') as f:
+    long_description = f.read()
+
+class UploadCommand(Command):
+    """ Build and publish this package.
+        Support setup.py upload. Copied from requests_html.
+    """
+
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in green color."""
+        print("\033[0;32m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        """ override
+        """
+        pass
+
+    def finalize_options(self):
+        """ override
+        """
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Publishing git tags…')
+        os.system('git tag v{0}'.format(__about__.__version__))
+        os.system('git push --tags')
+
+        sys.exit()
 
 install_requires = [
     "requests",
@@ -45,9 +93,13 @@ setup(
 #         dependency_links=dependency_links,
         extras_require={},
         entry_points={
-        'console_scripts': [
-            'httpdriver=httpdriver.cli:main_hrun',
-            'hdriver=httpdriver.cli:main_hrun',
-        ]
-    },
+            'console_scripts': [
+                'httpdriver=httpdriver.cli:main_hrun',
+                'hdriver=httpdriver.cli:main_hrun',
+            ]
+        },
+        # $ setup.py upload support.
+        cmdclass={
+            'upload': UploadCommand
+        }
     )
