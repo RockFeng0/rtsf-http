5bdaca4f74a69baa93d044eb57159728e913daa7
rockfeng0 <lkf20031988@163.com>
2020-11-11 12:21:25 +0800

    version 2.1.6

--- httpdriver/__about__.py
+++ httpdriver/__about__.py
@@ -21,7 +21,7 @@ UI and Web Http automation frame for python.
 
 __title__           = "rtsf-http"
 __short_desc__      = "only for http(s) test, base on rtsf"
-__version__         = "2.1.5"
+__version__         = "2.1.6"
 __autor__           = u"罗科峰"
 __author_email__    = "lkf20031988@163.com"
 __license__         = 'MIT'
